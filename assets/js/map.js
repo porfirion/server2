fd972783de1f4ca121a0f2e0ee1dd8fd7f7d89b3
Mikhail Vitsen <mikhailvitsen@gmail.com>
2015-11-23 02:19:10 +0300

    map works, viewport moving and scaling works

--- assets/js/map.js
+++ assets/js/map.js
@@ -11,17 +11,81 @@ function Map(elem) {
 		scale: 1,
 	};
 	this.points = [];
-	var _this = this;
+	this.objects = [];
+
+	this.gridSize = 100;
+
+	this.prevOffset = 0;
+
+	this.lastCursorPosition = null;
 
 	$(elem).on('click', function(event) {
-		_this.points.push({x: event.offsetX, y: event.offsetY, color: randomColor()});
-	})
+		console.log(event);
+		this.points.push({x: event.offsetX, y: event.offsetY, color: randomColor()});
+	}.bind(this));
+
+	$(elem).on('mousemove', function(ev) {
+		this.lastCursorPosition = {x: ev.offsetX, y: ev.offsetY};
+	}.bind(this));
+
+	$(elem).on('mouseenter', function(ev) {
+		
+	}.bind(this));
+
+	$(elem).on('mouseout', function(ev) {
+		this.lastCursorPosition = {x: this.elem.width / 2, y: this.elem.height / 2};
+	}.bind(this));
+
+	$(elem).on('mousewheel DOMMouseScroll', function(ev) {
+		var params = normalizeWheel(ev.originalEvent);
+		if (params.spinY > 0) {
+			// на себя
+			this.viewport.scale *= 1.1;
+		} else {
+			// от себя
+			this.viewport.scale *= 0.95;
+		}
+		console.log(params);
+		// console.log(ev.wheelDelta);
+		// console.log(ev.detail);
+		// console.log(ev.originalEvent.wheelDelta);
+		// console.log(ev.originalEvent.detail);
+		// console.log(ev);
+		return false;
+	}.bind(this));
+
+	this.isAnimating = false;
+
+	this.objects.push({
+		x: 0, 
+		y: 0,
+		color: 'red',
+		size: 20,
+	});
+	this.objects.push({
+		x: 100, 
+		y: 0,
+		color: 'green',
+		size: 50,
+	});
 }
 
 Map.prototype.draw = function() {
-	var rectSize = 50;
+	if (this.isAnimating) {
+		return;
+	}
+	
+	this.isAnimating = true;
+	this._draw();
+}
+
+Map.prototype._draw = function() {
 	var ctx = this.ctx;
 	var elem = this.elem;
+
+	if (this.lastCursorPosition == null) {
+		this.lastCursorPosition = {x: this.elem.width / 2, y: this.elem.height / 2};
+	}
 
 	elem.width = elem.clientWidth;
 	elem.height = elem.clientHeight;
@@ -35,6 +99,99 @@ Map.prototype.draw = function() {
 
 	ctx.clearRect(0, 0, ctx.width, ctx.height);
 	ctx.font = "16px serif";
+
+	this.adjustViewport();
+
+	this.drawGrid();
+
+	this.drawObjects();
+
+	// this.drawAnchors();
+
+	window.requestAnimationFrame(this._draw.bind(this));
+}
+
+Map.prototype.adjustViewport = function() {
+	this.viewport.x += (this.lastCursorPosition.x - this.elem.width / 2) * this.viewport.scale * 0.01;
+	this.viewport.y -= (this.lastCursorPosition.y - this.elem.height / 2) * this.viewport.scale * 0.01;
+}
+
+
+Map.prototype.drawObjects = function() {
+	var ctx = this.ctx;
+	ctx.save();
+	for (var i = 0; i < this.objects.length; i++) {
+		var obj = this.objects[i];
+		ctx.beginPath();
+		ctx.strokeStyle = obj.color;
+		var vp = this.realToViewport(obj);
+		var os = (obj.size) / this.viewport.scale;
+		ctx.rect(vp.x - os / 2, vp.y - os / 2, os, os);
+		ctx.stroke();
+	}
+	ctx.restore();
+}
+
+Map.prototype.drawGrid = function() {
+	var viewportW = this.elem.width;
+	var viewportH = this.elem.height;
+
+	var real = this.getRealViewport();
+	var viewGridSize = this.gridSize / this.scale;
+
+	var ctx = this.ctx;
+
+	var leftCol = Math.ceil((real.x - real.w / 2) / this.gridSize) * this.gridSize;
+	var topRow = Math.ceil((real.y - real.h / 2) / this.gridSize) * this.gridSize;
+
+
+	ctx.save();
+
+	ctx.save();
+	ctx.strokeStyle = 'magenta';
+	ctx.setLineDash([10, 4]);
+	this.prevOffset = (this.prevOffset + 0.5) % 14;
+	ctx.lineDashOffset = this.prevOffset;
+	ctx.beginPath();
+	// ctx.ellipse(this.lastCursorPosition.x, this.lastCursorPosition.y, 20, 20, 0, 0, Math.PI * 2);
+	ctx.arc(this.lastCursorPosition.x, this.lastCursorPosition.y, 20, 0, Math.PI * 2);
+	ctx.stroke();
+	ctx.restore();
+
+	ctx.strokeStyle = 'lime';
+	ctx.beginPath();
+	// ctx.ellipse(viewportW / 2, viewportH / 2, 10, 10, 0, 0, Math.PI * 2);
+	ctx.arc(viewportW / 2, viewportH / 2, 10, 0, Math.PI * 2);
+	ctx.stroke();
+
+	ctx.strokeStyle = '#ccc';
+	for (var i = 0; i < (real.w / this.gridSize); i++) {
+		var x = this.xRealToView(leftCol + i * this.gridSize);
+		ctx.beginPath();
+		ctx.moveTo(x, 0);
+		ctx.lineTo(x, viewportH);
+		ctx.stroke();
+	}
+
+	for (var j = 0; j < (real.h / this.gridSize); j++) {
+		var y = this.yRealToView(topRow + j * this.gridSize);
+		ctx.beginPath();
+		ctx.moveTo(0, y);
+		ctx.lineTo(viewportW, y);
+		ctx.stroke();	
+	}
+
+	ctx.restore();
+
+}
+
+Map.prototype.drawAnchors = function() {
+	var ctx = this.ctx;
+	var elem = this.elem;
+
+	ctx.save();
+
+	ctx.globalAlpha = 0.2;
 
 	ctx.beginPath();
 	ctx.rect(10, 10, elem.width - 20, elem.height - 20);
@@ -64,13 +221,16 @@ Map.prototype.draw = function() {
 	ctx.fillText('300*300', 300, 325);
 
 	ctx.beginPath();
-	ctx.ellipse(300, 150, 10, 10, 0, 0, Math.PI * 2);
+	// ctx.ellipse(300, 150, 10, 10, 0, 0, Math.PI * 2);
+	ctx.arc(300, 150, 10, 0, Math.PI * 2);
 	ctx.stroke();
 	ctx.beginPath();
-	ctx.ellipse(150, 300, 10, 10, 0, 0, Math.PI * 2);
+	// ctx.ellipse(150, 300, 10, 10, 0, 0, Math.PI * 2);
+	ctx.arc(150, 300, 10, 0, Math.PI * 2);
 	ctx.stroke();
 	ctx.beginPath();
-	ctx.ellipse(300, 300, 10, 10, 0, 0, Math.PI * 2);
+	// ctx.ellipse(300, 300, 10, 10, 0, 0, Math.PI * 2);
+	ctx.arc(300, 300, 10, 0, Math.PI * 2);
 	ctx.stroke();
 
 	ctx.beginPath();
@@ -84,13 +244,15 @@ Map.prototype.draw = function() {
 	ctx.moveTo(0, 0);
 	ctx.lineTo(elem.width, elem.height);
 
-	ctx.ellipse(elem.width, elem.height, 50, 50, 50, 50, 0);
+	// ctx.ellipse(elem.width, elem.height, 50, 50, 50, 50, 0);
+	ctx.arc(elem.width, elem.height, 50, 50, 0);
 	ctx.stroke();
 
 	ctx.strokeText('width*height', elem.width, elem.height);
 
 	ctx.beginPath();
-	ctx.ellipse(elem.width, elem.height, 20, 20, 0, 0, Math.PI * 2);
+	// ctx.ellipse(elem.width, elem.height, 20, 20, 0, 0, Math.PI * 2);
+	ctx.arc(elem.width, elem.height, 20, 0, Math.PI * 2);
 	ctx.stroke();
 	
 	ctx.beginPath();
@@ -103,13 +265,56 @@ Map.prototype.draw = function() {
 	for (var i = 0; i < this.points.length; i++) {
 		ctx.beginPath();
 		ctx.strokeStyle = this.points[i].color;
-		ctx.ellipse(this.points[i].x, this.points[i].y, 5, 5, 0, 0, Math.PI * 2);
+		// ctx.ellipse(this.points[i].x, this.points[i].y, 5, 5, 0, 0, Math.PI * 2);
+		ctx.arc(this.points[i].x, this.points[i].y, 5, 0, Math.PI * 2);
 		// ctx.closePath();
 		ctx.stroke();
 	}
 
+	ctx.restore();
+}
 
-	window.requestAnimationFrame(this.draw.bind(this));
+Map.prototype.xRealToView = function(xr) {
+	var viewportW = this.elem.width;
+	return viewportW / 2 + (xr - this.viewport.x) * (1 / this.viewport.scale);
+}
+
+Map.prototype.yRealToView = function(yr) {
+	var viewportH = this.elem.height;
+	return viewportH / 2 - (yr - this.viewport.y) * (1 / this.viewport.scale)
+}
+
+Map.prototype.viewportToReal = function(pos) {
+	var viewportW = this.elem.width;
+	var viewportH = this.elem.height;
+	var realPos = {
+		x: this.viewport.x + (pos.x - viewportW / 2) * this.viewport.scale,
+		y: this.viewport.y - (pos.y - viewportH / 2) * this.viewport.scale
+	}
+	return realPos;
+}
+
+Map.prototype.realToViewport = function(pos) {
+	var viewportW = this.elem.width;
+	var viewportH = this.elem.height;
+	var viewPos = {
+		x: viewportW / 2 + (pos.x - this.viewport.x) * (1 / this.viewport.scale),
+		y: viewportH / 2 - (pos.y - this.viewport.y) * (1 / this.viewport.scale)
+	}
+
+	return viewPos;
+}
+
+Map.prototype.getRealViewport = function() {
+	var viewportW = this.elem.width;
+	var viewportH = this.elem.height;
+
+	return {
+		x: this.viewport.x, 
+		y: this.viewport.y, 
+		w: viewportW * this.viewport.scale,
+		h: viewportH * this.viewport.scale,
+	}
 }
 
 function randomColor() {
@@ -117,4 +322,51 @@ function randomColor() {
 		+ (Math.round(Math.random() * 255)).toString(16)
 		+ (Math.round(Math.random() * 255)).toString(16)
 		+ (Math.round(Math.random() * 255)).toString(16);
+}
+
+// Reasonable defaults
+var PIXEL_STEP  = 10;
+var LINE_HEIGHT = 40;
+var PAGE_HEIGHT = 800;
+
+function normalizeWheel(/*object*/ event) /*object*/ {
+  var sX = 0, sY = 0,       // spinX, spinY
+      pX = 0, pY = 0;       // pixelX, pixelY
+
+  // Legacy
+  if ('detail'      in event) { sY = event.detail; }
+  if ('wheelDelta'  in event) { sY = -event.wheelDelta / 120; }
+  if ('wheelDeltaY' in event) { sY = -event.wheelDeltaY / 120; }
+  if ('wheelDeltaX' in event) { sX = -event.wheelDeltaX / 120; }
+
+  // side scrolling on FF with DOMMouseScroll
+  if ( 'axis' in event && event.axis === event.HORIZONTAL_AXIS ) {
+    sX = sY;
+    sY = 0;
+  }
+
+  pX = sX * PIXEL_STEP;
+  pY = sY * PIXEL_STEP;
+
+  if ('deltaY' in event) { pY = event.deltaY; }
+  if ('deltaX' in event) { pX = event.deltaX; }
+
+  if ((pX || pY) && event.deltaMode) {
+    if (event.deltaMode == 1) {          // delta in LINE units
+      pX *= LINE_HEIGHT;
+      pY *= LINE_HEIGHT;
+    } else {                             // delta in PAGE units
+      pX *= PAGE_HEIGHT;
+      pY *= PAGE_HEIGHT;
+    }
+  }
+
+  // Fall-back if spin cannot be determined
+  if (pX && !sX) { sX = (pX < 1) ? -1 : 1; }
+  if (pY && !sY) { sY = (pY < 1) ? -1 : 1; }
+
+  return { spinX  : sX,
+           spinY  : sY,
+           pixelX : pX,
+           pixelY : pY };
 }
